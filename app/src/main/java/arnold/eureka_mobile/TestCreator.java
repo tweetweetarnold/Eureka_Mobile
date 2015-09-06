f58c8026e5af2878e4e0c2b84fa583032058c38e
tweetweetarnold <arnold.lee.wt@gmail.com>
2015-09-06 22:18:29 +0800

    - added new test items in testcreator

--- app/src/main/java/arnold/eureka_mobile/TestCreator.java
+++ app/src/main/java/arnold/eureka_mobile/TestCreator.java
@@ -19,7 +19,7 @@ public class TestCreator {
 
     public static ArrayList<Canteen> getTestCanteenList(){
         ArrayList<Canteen> list = new ArrayList<>();
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 3; i++){
             list.add(new Canteen("Canteen " + i, i + " Street", null, new Date(), getTestHawkerList()));
         }
         return list;
@@ -27,9 +27,9 @@ public class TestCreator {
 
     public static ArrayList<Hawker> getTestHawkerList(){
         ArrayList<Hawker> list = new ArrayList<>();
-        list.add(new Hawker("hawker1", "123", "Hawker 1", 123, null, new Date(), getTestFoodList(), null));
-        list.add(new Hawker("hawker2", "123", "Hawker 2", 123, null, new Date(), getTestFoodList(), null));
-        list.add(new Hawker("hawker3", "123", "Hawker 3", 123, null, new Date(), getTestFoodList(), null));
+        for(int i = 0; i < 10; i++){
+            list.add(new Hawker("hawker" + i, "123", "Hawker " + i, 123, null, new Date(), getTestFoodList(), null));
+        }
         return list;
     }
 
@@ -38,6 +38,12 @@ public class TestCreator {
         list.add(new Food("Chicken Rice", "Rice", 3.00, null, new Date()));
         list.add(new Food("Wanton Mee", "Noodle", 4.50, null, new Date()));
         list.add(new Food("Abalone", "Seafood", 6.50, null, new Date()));
+        list.add(new Food("Crab", "Seafood", 6.50, null, new Date()));
+        list.add(new Food("Lobster", "Seafood", 6.50, null, new Date()));
+        list.add(new Food("Carrot Cake", "Cake", 6.50, null, new Date()));
+        list.add(new Food("Ba Chor Mee", "Noodle", 6.50, null, new Date()));
+        list.add(new Food("Laksa", "Noodle", 6.50, null, new Date()));
+        list.add(new Food("Smelly Cat", "Exotic", 6.50, null, new Date()));
         return list;
     }
 
